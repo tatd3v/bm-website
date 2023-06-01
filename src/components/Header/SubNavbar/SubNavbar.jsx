7a5424aac2061b7c8f3621095f63128920464ac7
tats-devteam <tatianad@eliteautomationgroup.com>
2023-06-01 01:51:55 -0500

    hide unused navbar options

--- src/components/Header/SubNavbar/SubNavbar.jsx
+++ src/components/Header/SubNavbar/SubNavbar.jsx
@@ -10,13 +10,13 @@ export const SubNavbar = () => {
       <Navbar id="responsive-navbar-nav" variant="dark">
         <Nav className="sub-navbar__container">
           <Nav.Link>Calendario</Nav.Link>
-          <NavDropdown title="Balls" id="collasible-nav-dropdown">
+          {/*<NavDropdown title="Balls" id="collasible-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Fotos</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Vdeos</NavDropdown.Item>
-            {/* <NavDropdown.Divider />
+              <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">
               Separated link
-            </NavDropdown.Item> */}
+            </NavDropdown.Item>
           </NavDropdown>
           <Nav.Link>Historia</Nav.Link>
           <Nav.Link>Categorias</Nav.Link>
@@ -26,7 +26,7 @@ export const SubNavbar = () => {
           <Nav.Link>Podcast</Nav.Link>
           <Nav.Link>Diccionario</Nav.Link>
           <Nav.Link>Aliados</Nav.Link>
-          <Nav.Link>Contacto</Nav.Link>
+          <Nav.Link>Contacto</Nav.Link>*/}
         </Nav>
       </Navbar>
     </>
