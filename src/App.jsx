fa98a9b42372cc0b4271d5adabf252bebc35f83c
tats-devteam <tatianad@eliteautomationgroup.com>
2023-06-01 09:05:34 -0500

    change the way to render the child

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import "./App.css";
+import './App.css';
 
 // @components
-import { BackgroundVideo, Header } from "./components";
+import { BackgroundVideo, Header } from './components';
 
 // @pages
-import { Home } from "./pages";
+import { Home } from './pages';
 
 function App() {
   return (
     <div className="App">
       {/* <ThemeProvider enableSystem={false} disableTransitionOnChange>
       <SiteContextProvider data={{ ...data?.site }}> */}
-      <BackgroundVideo blur={1} children={[<Header />]} />
+      <BackgroundVideo blur={1}>
+        <Header />
+      </BackgroundVideo>
       <Home />
       {/* </SiteContextProvider>
     </ThemeProvider> */}
