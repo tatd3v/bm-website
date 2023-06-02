12069e654538c011427c4f48fe1d4384002c0b28
tats-devteam <tatianad@eliteautomationgroup.com>
2023-06-02 05:11:39 -0500

    remove react refresh

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslintPlugin(), react(), reactRefresh()],
+  plugins: [eslintPlugin(), react()],
 });
