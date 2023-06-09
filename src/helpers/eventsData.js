e8771d696ded65537b65326378eeddab0dcd111c
tats-devteam <tatianad@eliteautomationgroup.com>
2023-06-09 00:05:35 -0500

    create evenData helper

---
+++ src/helpers/eventsData.js
@@ -0,0 +1,57 @@
+export const eventsData = [
+  {
+    date: new Date(2023, 5, 18).toISOString(),
+    id: 1,
+    image: {
+      alt: 'matrix-mini-kiki-ball',
+      src: 'https://i.ibb.co/7bPrMvy/matrix-mini-kiki-ball.jpg',
+    },
+    title: 'MATRIX',
+    type: 'Mini Kiki Ball',
+  },
+  {
+    date: new Date(2023, 6, 2).toISOString(),
+    id: 2,
+    image: {
+      alt: 'pride-kiki-ball',
+      src: 'https://i.ibb.co/bXj2PRK/pride-kiki-ball.jpg',
+    },
+    title: 'Pride 2023',
+    type: 'Kiki Ball',
+  },
+  {
+    date: new Date(2023, 7, 5).toISOString(),
+    id: 3,
+    image: {},
+    title: 'Miloy 007',
+    type: 'Vogue Night',
+  },
+  {
+    date: new Date(2023, 8, 16).toISOString(),
+    id: 4,
+    image: {},
+    title: 'House of Prince & House of Cazul',
+    type: 'Kiki Ball',
+  },
+  {
+    date: new Date(2023, 9, 15).toISOString(),
+    id: 5,
+    image: {},
+    title: 'House of Druzhinin',
+    type: 'Kiki Ball',
+  },
+  {
+    date: new Date(2023, 10, 18).toISOString(),
+    id: 6,
+    image: {},
+    title: 'House of Mattel',
+    type: 'Kiki Ball',
+  },
+  {
+    date: new Date(2023, 11, 16).toISOString(),
+    id: 7,
+    image: {},
+    title: 'Fin de Año',
+    type: 'Kiki Ball',
+  },
+];
