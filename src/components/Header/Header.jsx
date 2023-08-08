bdf175cfe5bb103458c2d14ad7a1ebb694017482
tats-devteam <tatianad@eliteautomationgroup.com>
2023-08-08 07:15:54 -0500

    add toggle for mobil resolutions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,19 +8,22 @@ import { SubNavbar } from './SubNavbar';
 import Logo from '../../assets/LogoWhite.png';
 
 // @styles
-import './header.css';
+import './header.scss';
 
 const Header = () => {
   return (
     <Navbar className="header__navbar" bg="transparent" expand="lg">
       <Container className="header__nav-container" fluid>
-        <Navbar.Brand href="/" className="header__navbar-brand my-lg-0">
-          <img
-            src={Logo}
-            alt="Logo"
-            style={{ maxWidth: '70%', maxHeight: '200px' }}
-          />
-        </Navbar.Brand>
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <Navbar.Brand href="/" className="header__navbar-brand my-lg-0">
+            <img
+              src={Logo}
+              alt="Logo"
+              style={{ maxWidth: '70%', maxHeight: '200px' }}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+        </div>
         <SubNavbar />
       </Container>
     </Navbar>
