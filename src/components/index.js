7d6bc94d2218b4eddd49c40d3d2c0b09f1e7c4dc
tats-devteam <tatianad@eliteautomationgroup.com>
2023-08-23 14:01:52 -0500

    move searchBox component directory root

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 import { BackgroundVideo } from './backgroundVideo';
 import { CalendarEvent } from './Modals';
-import { Dictionary } from "./Dictionary"
+import { Dictionary } from './Dictionary';
 import { EventsCalendar } from './Calendar';
 import { Header } from './Header';
-import { SearchBox } from './ui';
 
-export { BackgroundVideo, CalendarEvent, Dictionary, EventsCalendar, Header, SearchBox };
+export { BackgroundVideo, CalendarEvent, Dictionary, EventsCalendar, Header };
