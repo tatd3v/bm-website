cbc8d8f822fdd944d7d01600e36c38b3baa082db
tats-devteam <tatianad@eliteautomationgroup.com>
2023-09-06 23:02:31 -0500

    Implement isMobile redux variable in subnavbar component

--- src/components/Header/SubNavbar/SubNavbar.jsx
+++ src/components/Header/SubNavbar/SubNavbar.jsx
@@ -1,5 +1,5 @@
 // @vendors
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
 // @styles
@@ -9,46 +9,44 @@ import './subNavbar.scss';
 import {
   setShowCalendar,
   setShowDictionary,
-  setShowSubNavbar,
+  //setShowSubNavbar,
 } from '../../../app';
 
 export const SubNavbar = () => {
   const dispatch = useDispatch();
 
-  const { showSubNavbar } = useSelector((state) => state.ui.header);
+  const { header, isMobile } = useSelector((state) => state.ui);
+  const { showSubNavbar } = header;
 
   const onClickCalendar = () => {
     dispatch(setShowCalendar(true));
     dispatch(setShowDictionary(false));
-    dispatch(setShowSubNavbar());
   };
 
   const onClickDictionary = () => {
     dispatch(setShowCalendar(false));
     dispatch(setShowDictionary(true));
-    dispatch(setShowSubNavbar());
   };
 
   return (
     <>
-      {showSubNavbar && (
-        <Navbar.Collapse
-          className={`sub-navbar__collapse ${showSubNavbar ? 'show' : ''}`}
-          id="navbarScroll"
-          variant="dark"
-        >
-          <Nav className="sub-navbar__container">
-            <Nav.Link onClick={onClickCalendar}>Calendario</Nav.Link>
-            <Nav.Link onClick={onClickDictionary}>Diccionario</Nav.Link>
-            {/*<NavDropdown title="Balls" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Fotos</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Vdeos</NavDropdown.Item>
-              <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
+      <Navbar.Collapse
+        className={`sub-navbar__collapse ${
+          (showSubNavbar && isMobile) || !isMobile ? 'show' : ''
+        }`}
+        id="navbarScroll"
+        variant="dark"
+      >
+        <Nav className="sub-navbar__container">
+          <Nav.Link onClick={onClickCalendar}>Calendario</Nav.Link>
+          <Nav.Link onClick={onClickDictionary}>Diccionario</Nav.Link>
+          <NavDropdown title="Balls" id="collasible-nav-dropdown">
+            <NavDropdown.Item>Fotos</NavDropdown.Item>
+            <NavDropdown.Item>Vdeos</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item>Separated link</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link>Historia</Nav.Link>
+          {/*<Nav.Link>Historia</Nav.Link>
           <Nav.Link>Categorias</Nav.Link>
           <Nav.Link>Identidades</Nav.Link>
           <Nav.Link>Portafolios</Nav.Link>
@@ -56,9 +54,8 @@ export const SubNavbar = () => {
           <Nav.Link>Podcast</Nav.Link>
           <Nav.Link>Aliados</Nav.Link>
           <Nav.Link>Contacto</Nav.Link>*/}
-          </Nav>
-        </Navbar.Collapse>
-      )}
+        </Nav>
+      </Navbar.Collapse>
     </>
   );
 };
