a5a07c2574c6163b3fbdb6a67294c928c688d6c8
tats-devteam <tatianad@eliteautomationgroup.com>
2023-09-11 23:57:06 -0500

    add browser router react router component to the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+// @vendorsS
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 // @src
 import './index.css';
@@ -12,8 +14,10 @@ import { store } from './app';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
