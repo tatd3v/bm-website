21ebeaaad4e81f351d6a4d5fbde5afa0d1d12524
tats-devteam <tatianad@eliteautomationgroup.com>
2023-09-13 16:05:14 -0500

    create Gallery page

---
+++ src/pages/Gallery/Gallery.jsx
@@ -0,0 +1,14 @@
+// @vendors
+import { Container } from 'react-bootstrap';
+
+// @components
+import { EventGallery } from '../../components/Gallery';
+
+export const Gallery = () => {
+  return (
+    <Container>
+      <h1>GALERIA BALLROOM</h1>
+      <EventGallery />
+    </Container>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { Gallery } from './Gallery';
 import { Home } from './Home';
 
-export { Home };
+export { Gallery, Home };
