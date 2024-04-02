c91927a00f40ab53503ded27e2827c221f71ec2f
tatadavila <tata.davila.a@gmail.com>
2024-04-02 03:06:35 -0500

    create stickyicon component

---
+++ src/components/BottomSticky/StickyIcon.jsx
@@ -0,0 +1,11 @@
+// @vendors
+import { Container } from "react-bootstrap"
+
+export const StickyIcon = () => {
+
+  return (
+    <Container className="sticki-icon_container">
+      <img src={} alt={} />
+    </Container>
+  )
+}
