117e5448825f20290cff06c22629b51ac5e5f3a8
tatadavila <tata.davila.a@gmail.com>
2024-04-02 03:13:08 -0500

    add index file in images folder

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import instagramLogo from './igTransparentLogo.png';
+import tikTokLogo from './ttTransparentLogo.png';
+import youtubeLogo from './igTransparentLogo.png';
+
+export { instagramLogo, tikTokLogo, youtubeLogo };
