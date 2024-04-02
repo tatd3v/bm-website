83899fa32f41fa35e472992c62fad1759a86579a
tatadavila <tata.davila.a@gmail.com>
2024-04-02 03:09:55 -0500

    create icondata file

---
+++ src/components/BottomSticky/iconsData.js
@@ -0,0 +1,7 @@
+import { instagramLogo, tikTokLogo, youtubeLogo } from '../../assets/images';
+
+export const logos = [
+  { name: 'instagram logo', image: { instagramLogo } },
+  { name: 'tiktok logo', image: { tikTokLogo } },
+  { name: 'youtube logo', image: { youtubeLogo } },
+];
