85f795d3b5fc9a19e2538771a143f3ec497143ca
tatadavila <tata.davila.a@gmail.com>
2024-04-02 03:04:54 -0500

    create stickynav component

--- src/components/BottomSticky/StickyNav.jsx
+++ src/components/BottomSticky/StickyNav.jsx
@@ -1,10 +1,29 @@
+// @vendors
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+// @app
+import { setSocialMedia } from '../../app';
+
+// @components
+import { StickyIcon } from './StickyIcon';
+
+// @data
+import { logos } from './iconsData';
+
 // @styles
-import 'stickyNav.scss'
+import './stickyNav.scss';
 
 export const StickyNav = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSocialMedia(logos));
+  }, []);
+
   return (
-    <div className="bs_sticky-nav-container">
-      
+    <div className="sn_sticky-nav-container">
+      <StickyIcon />
     </div>
-  )
-}
+  );
+};
