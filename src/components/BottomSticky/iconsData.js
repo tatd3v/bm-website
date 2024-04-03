45a6eb6a5808be889aeea57986425dd657603d0a
tatadavila <tata.davila.a@gmail.com>
2024-04-03 02:46:33 -0500

    add url key into iconsdata object

--- src/components/BottomSticky/iconsData.js
+++ src/components/BottomSticky/iconsData.js
@@ -1,7 +1,19 @@
 import { instagramLogo, tikTokLogo, youtubeLogo } from '../../assets/images';
 
 export const logos = [
-  { name: 'instagram logo', image: { instagramLogo } },
-  { name: 'tiktok logo', image: { tikTokLogo } },
-  { name: 'youtube logo', image: { youtubeLogo } },
+  {
+    name: 'instagram logo',
+    source: instagramLogo,
+    url: 'https://www.instagram.com/ballroomedellin__',
+  },
+  {
+    name: 'tiktok logo',
+    source: tikTokLogo,
+    url: 'https://www.tiktok.com/@ballroommedellin',
+  },
+  {
+    name: 'youtube logo',
+    source: youtubeLogo,
+    url: 'https://www.youtube.com/channel/UCKe_uyAQh4RD3yFIMmi-SMA',
+  },
 ];
