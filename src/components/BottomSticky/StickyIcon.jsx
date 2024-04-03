5cfba5620fe763e05ef1caadaeb3189b49f22608
tatadavila <tata.davila.a@gmail.com>
2024-04-03 03:09:30 -0500

    modify stickyicon component

--- src/components/BottomSticky/StickyIcon.jsx
+++ src/components/BottomSticky/StickyIcon.jsx
@@ -1,11 +1,34 @@
 // @vendors
-import { Container } from "react-bootstrap"
+import { useState } from 'react';
 
-export const StickyIcon = () => {
+// @styles
+import './stickyIcon.scss';
+
+export const StickyIcon = ({ name, source, url }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+    window.open(url, 'chromeTab');
+    setClicked(false);
+    dispatchEvent(new MouseEvent('click', { shiftKey: true }));
+  };
 
   return (
-    <Container className="sticki-icon_container">
-      <img src={} alt={} />
-    </Container>
-  )
-}
+    <div
+      className={
+        clicked ? 'sticky-icon_container clicked' : 'sticky-icon_container'
+      }
+    >
+      <img
+        alt={name}
+        className={
+          clicked ? 'si_social-media-logo clicked' : 'si_social-media-logo'
+        }
+        src={`../../${source}`}
+        loading="lazy"
+        onClick={handleClick}
+      />
+    </div>
+  );
+};
