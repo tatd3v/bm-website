d09e797f67c0efc1f52534d833549988cd3cefab
Tats D <tatdev91@gmail.com>
2024-04-25 00:06:30 -0500

    Modify liveradio component

--- src/components/LiveRadio/LiveRadio.jsx
+++ src/components/LiveRadio/LiveRadio.jsx
@@ -1,11 +1,13 @@
-// @styles
-import "./liveRadio.scss"
-
 export const LiveRadio = () => {
   return (
-    <div>
-      {/* <iframe src="https://64.23.235.37/public/shade_radio/embed?theme=dark" style={{ width: '100%', maxHeight: '100px', border: '0', borderWidth: '0' }}></iframe> */}
-      <iframe src="https://64.23.235.37/public/shade_radio/embed?theme=dark" style={{ width: '100%', minHeight: '150px', border: '0' }}></iframe>
-    </div>
-  )
-}
+    <iframe
+      src="http://64.23.235.37/public/shade_radio/embed?theme=dark"
+      style={{
+        background: 'none transparent !important',
+        border: '0',
+        maxHeight: '40px',
+        width: '100%',
+      }}
+    ></iframe>
+  );
+};
