48b8c1f1dae5b51b8ba1485011062a7535f2265b
Tats D <tatdev91@gmail.com>
2024-04-25 00:08:16 -0500

    Crate loading slide

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,18 @@ import { BackgroundVideo } from './backgroundVideo';
 import { CalendarEvent } from './Modals';
 import { Dictionary } from './Dictionary';
 import { EventsCalendar } from './Calendar';
+import { Footer } from './Footer';
 import { Header } from './Header';
 import { LiveRadio } from './LiveRadio';
+import { Loading } from './Loading';
 
-export { BackgroundVideo, CalendarEvent, Dictionary, EventsCalendar, Header, LiveRadio };
+export {
+  BackgroundVideo,
+  CalendarEvent,
+  Dictionary,
+  EventsCalendar,
+  Footer,
+  Header,
+  LiveRadio,
+  Loading,
+};
