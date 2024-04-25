cc561570071d5379a1525e2bc159bc4bb17a8c7d
Tats D <tatdev91@gmail.com>
2024-04-25 00:07:52 -0500

    create loading component

---
+++ src/components/Loading/LoadingWord.jsx
@@ -0,0 +1,19 @@
+// @styles
+import './loadingWords.scss';
+
+export const LoadingWord = () => {
+  return (
+    <div className="loading">
+      <div className="loading__letter">L</div>
+      <div className="loading__letter">o</div>
+      <div className="loading__letter">a</div>
+      <div className="loading__letter">d</div>
+      <div className="loading__letter">i</div>
+      <div className="loading__letter">n</div>
+      <div className="loading__letter">g</div>
+      <div className="loading__letter">.</div>
+      <div className="loading__letter">.</div>
+      <div className="loading__letter">.</div>
+    </div>
+  );
+};
