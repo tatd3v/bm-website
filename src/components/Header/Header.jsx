6ed885bd3b6b647ec1c650694dd53262e01ffcf3
Tats D <tatdev91@gmail.com>
2024-04-29 21:36:19 -0500

    modify header style file name and add styles for menu toggler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+// @vendors
 import { Container, Navbar } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+
+// @app
+import { setShowSubNavbar } from '../../app';
+
+// @assets
+import Logo from '../../assets/ShortLogoWhite.png';
 
 // @components
 import { SubNavbar } from './SubNavbar';
 import { SocialMediaNav } from './SocialMediaNavBar';
 
-// @assets
-import Logo from '../../assets/ShortLogoWhite.png';
-
 // @styles
-import './header.scss';
-import { useDispatch } from 'react-redux';
-
-// @app
-import { setShowSubNavbar } from '../../app';
+import './_header.scss';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -23,27 +23,25 @@ const Header = () => {
   };
 
   return (
-    <Navbar className="header__navbar bg-light" bg="transparent" expand="lg">
-      <Container className="header__nav-container" fluid>
-        <div className="d-flex flex-column align-items-center w-100">
-          <div className="header_nav-logo-toggle-container d-flex align-items-center w-100">
-            <Navbar.Brand className="header__navbar-brand my-lg-0">
-              <img
-                src={Logo}
-                alt="Logo"
-                style={{ maxWidth: '75%', maxHeight: '130px' }}
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle
-              aria-controls="navbarScroll"
-              style={{
-                borderColor: 'white',
-              }}
-              onClick={toggleSubNavbar}
-            />
-          </div>
-          <SocialMediaNav />
+    <Navbar
+      className="header__navbar bg-light w-100 main-font-style pt-3"
+      bg="transparent"
+      expand="lg"
+    >
+      <Container
+        className="header__nav-container d-flex flex-column justify-content-between align-items-center p-0 w-100"
+        fluid
+      >
+        <div className="header_nav-logo-toggle-container d-flex justify-content-center align-items-center position-relative">
+          <Navbar.Brand className="header__navbar-brand d-flex flex-column justify-content-between align-items-center my-lg-0 mx-0 p-0">
+            <img src={Logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="navbarScroll"
+            onClick={toggleSubNavbar}
+          />
         </div>
+        <SocialMediaNav />
         <SubNavbar />
       </Container>
     </Navbar>
