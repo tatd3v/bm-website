af30db5c1381fb39ab69de3dada44c399ee8df8c
Tats D <tatdev91@gmail.com>
2024-05-09 03:14:21 -0500

    modify styles liveradio and index scss

--- src/components/LiveRadio/LiveRadio.jsx
+++ src/components/LiveRadio/LiveRadio.jsx
@@ -1,20 +1,13 @@
-// @vendors
+// @styles
+import './_liveRadio.scss';
 
 export const LiveRadio = () => {
   return (
-      <iframe
-        allow="geolocation; microphone; camera; autoplay; encrypted-media"
-        sandbox="allow-same-origin allow-scripts"
-        src="https://radio.ballroomedellin.com/public/shade_radio/embed?theme=dark"
-        style={{
-          background: 'none transparent',
-          border: '0',
-          maxHeight: '100%',
-          maxWidth: '100%',
-          minHeight: '100%',
-          minWidth: '100%'
-        }}
-        title="Live Radio"
-      ></iframe>
+    <iframe
+      allow="geolocation; microphone; camera; autoplay; encrypted-media"
+      sandbox="allow-same-origin allow-scripts"
+      src="https://radio.ballroomedellin.com/public/shade_radio/embed?theme=dark"
+      title="Live Radio"
+    ></iframe>
   );
 };
