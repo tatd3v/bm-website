745f4c16a6349e865bc9811e70d9a27f6e1f5829
Tats D <tatdev91@gmail.com>
2024-05-09 07:56:06 -0500

    indent file

--- src/components/LiveRadio/LiveRadio.jsx
+++ src/components/LiveRadio/LiveRadio.jsx
@@ -1,20 +1,18 @@
-// @vendors
-
 export const LiveRadio = () => {
   return (
-      <iframe
-        allow="geolocation; microphone; camera; autoplay; encrypted-media"
-        sandbox="allow-same-origin allow-scripts"
-        src="https://radio.ballroomedellin.com/public/shade_radio/embed?theme=dark"
-        style={{
-          background: 'none transparent',
-          border: '0',
-          maxHeight: '100%',
-          maxWidth: '100%',
-          minHeight: '100%',
-          minWidth: '100%'
-        }}
-        title="Live Radio"
-      ></iframe>
+    <iframe
+      allow="geolocation; microphone; camera; autoplay; encrypted-media"
+      sandbox="allow-same-origin allow-scripts"
+      src="https://radio.ballroomedellin.com/public/shade_radio/embed?theme=dark"
+      style={{
+        background: 'none transparent',
+        border: '0',
+        maxHeight: '100%',
+        maxWidth: '100%',
+        minHeight: '100%',
+        minWidth: '100%',
+      }}
+      title="Live Radio"
+    ></iframe>
   );
 };
