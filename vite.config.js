f713964d63e5065180da9e6ccd21228bbacfe0ae
Tats D <tatdev91@gmail.com>
2024-05-10 03:35:37 -0500

    modify vite config file and create path for src folders

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,43 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
+import react from '@vitejs/plugin-react';
+import viteSvgLoader from 'vite-svg-loader';
+import url from '@rollup/plugin-url';
+import path from 'path';
 
 export default defineConfig({
-  assetsInclude: ['**/*.webm', '**/*.mp4', '**/*.ogv'],
-  plugins: [eslintPlugin(), react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx',
+      },
+      output: {
+        dir: 'dist',
+      },
+      plugins: [
+        eslintPlugin(),
+        react(),
+        url({
+          include: ['**/*.mp4', '**/*.webm'],
+          limit: Infinity,
+          emitFiles: true,
+          publicPath: '/videos/',
+        }),
+        viteSvgLoader(),
+      ],
+    },
+    assetsInclude: ['**/*.webm', '**/*.mp4', '**/*.ogv'],
+  },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src/'),
+      app: path.resolve(__dirname, './src/app/index.js'),
+      assets: path.resolve(__dirname, './src/assets/index.js'),
+      components: path.resolve(__dirname, './src/components/index.js'),
+      helpers: path.resolve(__dirname, './src/helpers/index.js'),
+      pages: path.resolve(__dirname, './src/pages/index.js'),
+      router: path.resolve(__dirname, './src/routes/index.js'),
+      styles: path.resolve(__dirname, './src/styles/index.js'),
     },
   },
   server: {
