7dc52890716b97212063429db54097995d6b7cb8
Tats D <tatdev91@gmail.com>
2024-05-10 02:46:45 -0500

    modify path erease dot

--- src/assets/videos/index.js
+++ src/assets/videos/index.js
@@ -1,4 +1,4 @@
-import backgroundVidMp4 from './background-video.mp4';
-import backgroundVidWebm from './background-video.webm';
+import backgroundVidMp4 from '/background-video.mp4';
+import backgroundVidWebm from '/background-video.webm';
 
 export { backgroundVidMp4, backgroundVidWebm };
