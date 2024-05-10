0db1c75ac350ac17180198f8b3cbca3613b80d16
Tats D <tatdev91@gmail.com>
2024-05-10 04:09:38 -0500

    modify webm video path

--- src/assets/videos/index.js
+++ src/assets/videos/index.js
@@ -1,4 +1,4 @@
 import backgroundVidMp4 from './background-video-mp4.mp4';
-import backgroundVidWebm from './background-video-webm.webm';
+import backgroundVidWebm from 'background-video-webm.webm';
 
 export { backgroundVidMp4, backgroundVidWebm };
