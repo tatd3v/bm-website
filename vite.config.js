b10a7b92e336e0203be9490e55dc6ca5a5036f9b
Tats D <tatdev91@gmail.com>
2024-05-10 02:12:12 -0500

    add assetsinclude config for vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.webm', '**/*.mp4', '**/*.ogv'],
   plugins: [eslintPlugin(), react()],
   server: {
-    host: true
-  }
+    host: true,
+  },
 });
