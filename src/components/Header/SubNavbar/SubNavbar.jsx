d23cff6c4b6c4e71a7d7445d3ec5b0a576ce494b
Tats D <tatdev91@gmail.com>
2024-05-16 01:30:38 -0500

    remove unused code from subnavbar component

--- src/components/Header/SubNavbar/SubNavbar.jsx
+++ src/components/Header/SubNavbar/SubNavbar.jsx
@@ -1,6 +1,5 @@
 // @vendors
-import { forwardRef, useEffect } from 'react';
-import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { Container, Nav, NavDropdown } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -8,41 +7,15 @@ import { Link } from 'react-router-dom';
 import './subNavbar.scss';
 
 // @app
-import {
-  setEventInfo,
-  setShowCalendar,
-  setShowDictionary,
-  setShowSubNavbar,
-} from '../../../app';
+import { setEventInfo, setShowCalendar, setShowDictionary } from '../../../app';
 
 // @helpers
 import { extractNumbersFromString } from '../../../helpers';
 
-const SubNavbar = forwardRef((props, ref) => {
+const SubNavbar = () => {
   const dispatch = useDispatch();
-  const { header, device } = useSelector((state) => state.ui);
-  const { isMobile } = device;
-  const { showSubNavbar } = header;
 
   const { eventsByYear } = useSelector((state) => state.data.calendar);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (showSubNavbar && !event.target.closest('.sub-navbar__collapse')) {
-        dispatch(setShowSubNavbar(false));
-      }
-    };
-
-    if (showSubNavbar && isMobile) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [dispatch, isMobile, showSubNavbar]);
 
   const onClickCalendar = () => {
     dispatch(setShowCalendar(true));
@@ -60,45 +33,43 @@ const SubNavbar = forwardRef((props, ref) => {
 
   return (
     <>
-      <Navbar.Collapse
-        className={`sub-navbar__collapse ${
-          showSubNavbar && isMobile ? 'show' : ''
-        }`}
-        id="navbarScroll"
-        variant="dark"
-      >
-        <Nav className="sub-navbar__container" ref={ref}>
-          <Nav.Link onClick={onClickCalendar}>Calendario</Nav.Link>
-          <Nav.Link onClick={onClickDictionary}>Diccionario</Nav.Link>
-          <NavDropdown title="Balls" id="collasible-nav-dropdown">
-            <>
-              {Object.keys(eventsByYear).map((year, index) => {
-                return (
-                  <div key={year}>
-                    <Container>
-                      <h4>{extractNumbersFromString(year)}</h4>
-                      {eventsByYear[year].map((event) => {
-                        return (
-                          <NavDropdown.Item
-                            key={event.id}
-                            as={Link}
-                            onClick={() => onClickEvent(event)}
-                            to={`gallery${event.path}`}
-                          >
-                            {`${event.title} ${event.type}`}
-                          </NavDropdown.Item>
-                        );
-                      })}
-                    </Container>
-                    {index !== Object.keys(eventsByYear).length - 1 && (
-                      <NavDropdown.Divider />
-                    )}
-                  </div>
-                );
-              })}
-            </>
-          </NavDropdown>
-          {/*<Nav.Link>Historia</Nav.Link>
+      <Nav className="sub-navbar__container">
+        <Nav.Link eventKey="1" onClick={onClickCalendar}>
+          Calendario
+        </Nav.Link>
+        <Nav.Link eventKey="2" onClick={onClickDictionary}>
+          Diccionario
+        </Nav.Link>
+        <NavDropdown title="Balls" id="collasible-nav-dropdown">
+          <>
+            {Object.keys(eventsByYear).map((year, index) => {
+              return (
+                <div key={year}>
+                  <Container>
+                    <h4>{extractNumbersFromString(year)}</h4>
+                    {eventsByYear[year].map((event) => {
+                      return (
+                        <NavDropdown.Item
+                          eventKey={`3.${event.id}`}
+                          key={event.id}
+                          as={Link}
+                          onClick={() => onClickEvent(event)}
+                          to={`gallery${event.path}`}
+                        >
+                          {`${event.title} ${event.type}`}
+                        </NavDropdown.Item>
+                      );
+                    })}
+                  </Container>
+                  {index !== Object.keys(eventsByYear).length - 1 && (
+                    <NavDropdown.Divider />
+                  )}
+                </div>
+              );
+            })}
+          </>
+        </NavDropdown>
+        {/*<Nav.Link>Historia</Nav.Link>
           <Nav.Link>Categorias</Nav.Link>
           <Nav.Link>Identidades</Nav.Link>
           <Nav.Link>Portafolios</Nav.Link>
@@ -106,11 +77,10 @@ const SubNavbar = forwardRef((props, ref) => {
           <Nav.Link>Podcast</Nav.Link>
           <Nav.Link>Aliados</Nav.Link>
           <Nav.Link>Contacto</Nav.Link>*/}
-        </Nav>
-      </Navbar.Collapse>
+      </Nav>
     </>
   );
-});
+};
 
 SubNavbar.displayName = 'SubNavbar';
 
