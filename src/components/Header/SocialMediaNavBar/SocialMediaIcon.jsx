54a78b80daa54a49fedfc54afa13d769632fb631
Tats D <tatdev91@gmail.com>
2024-06-15 18:43:10 -0500

    modify socialmedia icons code

--- src/components/Header/SocialMediaNavBar/SocialMediaIcon.jsx
+++ src/components/Header/SocialMediaNavBar/SocialMediaIcon.jsx
@@ -1,26 +1,17 @@
-// @vendors
-import { useState } from 'react';
-
 // @styles
 import './_socialMediaIcon.scss';
 
 export const SocialMediaIcon = ({ name, source, url }) => {
-  const [clicked, setClicked] = useState(false);
-
   const handleClick = () => {
-    setClicked(true);
     window.open(url, 'chromeTab');
-    setClicked(false);
     dispatchEvent(new MouseEvent('click', { shiftKey: true }));
   };
 
   return (
-    <div className={clicked ? 'smi_container clicked' : 'smi_container'}>
+    <div className="smi_container">
       <img
         alt={name}
-        className={
-          clicked ? 'smi_social-m edia-logo clicked' : 'smi_social-media-logo'
-        }
+        className="smi_social-media-logo"
         src={`../../${source}`}
         loading="lazy"
         onClick={handleClick}
