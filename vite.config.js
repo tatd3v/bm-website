167b3763ad6a11e40aba899fa8466e0d8b382407
Tats D <tatdev91@gmail.com>
2024-06-20 21:45:34 -0500

    implement enviromentplugin and add api alias

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-// @cendors
+// @vendors
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 import path from 'path';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 export default defineConfig({
-  plugins: [eslintPlugin(), react()],
+  plugins: [EnvironmentPlugin('all'), eslintPlugin(), react()],
   server: {
     host: true,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
       '@app': path.resolve(__dirname, './src/app'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
