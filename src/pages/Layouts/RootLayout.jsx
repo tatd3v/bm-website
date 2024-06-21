3cb91cef6dbab242a95ec0f8b9c3fed3f083c735
Tats D <tatdev91@gmail.com>
2024-06-20 21:58:10 -0500

    modify rootlayout in order to make it responsive

--- src/pages/Layouts/RootLayout.jsx
+++ src/pages/Layouts/RootLayout.jsx
@@ -3,32 +3,22 @@ import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 // @components
-import {
-  Dictionary,
-  EventsCalendar,
-  Header,
-  //FloatButton,
-  Footer,
-  StickyCalendar,
-} from '@components';
+import { Dictionary, Header, Footer, StickyCalendar } from '@components';
 
 export const RootLayout = () => {
-  const { showCalendar, showDictionary } = useSelector(
-    (state) => state.ui.calendar
-  );
+  const { showDictionary } = useSelector((state) => state.ui.calendar);
   return (
     <>
       <div className="rootlayout__container">
         <Header />
-        <main>
+        <main className="rootlayout__main">
           {/* <FloatButton /> */}
           <StickyCalendar />
-          {showCalendar && <EventsCalendar />}
           {showDictionary && <Dictionary />}
           <Outlet />
         </main>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
