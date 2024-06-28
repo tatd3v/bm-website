da6c4f9eb95e4f39115ddc591d1335bc53b49972
Tats D <tatdev91@gmail.com>
2024-06-28 09:31:28 -0500

    import home styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
 // @components
 import { PlaylistSidebar } from '@/components';
 
+// @styles
+import './_home.scss';
+
 export const HomePage = () => {
   return <div className="home__container">{<PlaylistSidebar />}</div>;
 };
