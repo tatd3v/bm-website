c920f344e80b84e6361323bc7e834a4c3e6878e8
tatd3v <tatdev91@gmail.com>
2025-01-26 02:15:00 -0500

    modify fetch for youtube playlists

--- src/api/Youtube/index.js
+++
@@ -1,3 +0,0 @@
-import { fetchMultiplePlaylists } from './youtubeApi';
-
-export { fetchMultiplePlaylists };
---
+++ src/api/youtube/getPlaylists.js
@@ -0,0 +1,22 @@
+// @vendors
+import axios from 'axios';
+
+const API_URL = process.env.VITE_API_URL;
+
+console.log(`${API_URL}api/v1/youtube/`)
+
+export async function fetchPlaylistsData() {
+  try {
+
+    const response = await axios.get(`${API_URL}api/v1/youtube/`, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch YouTube data:', error);
+    throw new Error('Failed to fetch YouTube data');
+  }
+}
