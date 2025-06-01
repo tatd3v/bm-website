f2c4394c8510c4fdf0575cb81240bd951fec4f61
Tats <tatdev91@gmail.com>
2025-05-31 23:57:47 -0500

    Merge Develop into Main (#57)
    * Feature telegram icon link (#56)
    
    * replace deprecated SASS @import for @use
    
    * add telegram icon png
    
    * create image import export
    
    * add telegram data to logos array
    
    * add as * for each use of scss file
    
    * change boostrap import path
    
    * add silence deprecation for sass legacy
    
    * update redux thunk and sass
    
    * xhanges on embed yt done

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // @src
 import App from './App';
