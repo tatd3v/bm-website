fc89c540ef537b27b6e2c9e3945e40be864a08cf
tatd3v <tatdev91@gmail.com>
2025-05-31 23:54:14 -0500

    xhanges on embed yt done

--- src/components/YoutubePlaylists/YoutubeEmbed.jsx
+++ src/components/YoutubePlaylists/YoutubeEmbed.jsx
@@ -1,21 +1,47 @@
 // @vendors
+import { memo, useMemo, useState, useEffect } from "react";
 import { useSelector } from 'react-redux';
+
+// @components
+import { Loading } from "../Loading";
 
 // @styles
 import './_youtubeEmbed.scss';
 
 const YoutubeEmbed = () => {
   const { currentVideo } = useSelector((state) => state.youtubePlaylist);
+  const hideLoading = useSelector((state) => state.ui.loading.hideLoading);
+
+  const videoUrl = useMemo(() => {
+    return currentVideo ? `https://www.youtube.com/embed/${currentVideo}` : null;
+  }, [currentVideo]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (videoUrl) {
+      setIsLoading(true);
+      const timer = setTimeout(() => setIsLoading(false), 1000); // Simulate loading delay
+      return () => clearTimeout(timer);
+    }
+  }, [videoUrl]);
+
+  if (!videoUrl) {
+    return <p className="ye__no-video">No video available</p>;
+  }
 
   return (
     <div className="ye__responsive-container" id="player">
+      {isLoading && !hideLoading ? <Loading /> : null}
       <iframe
-        src={`https://www.youtube.com/embed/${currentVideo}`}
+        src={videoUrl}
+        title="YouTube Video Player"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        onLoad={() => setIsLoading(false)}
       />
     </div>
   );
 };
 
-export default YoutubeEmbed;
+export default memo(YoutubeEmbed);
