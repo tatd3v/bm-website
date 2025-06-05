ecf1c707d5eddf9a1565195012bcbd2c995ea332
tatd3v <tatdev91@gmail.com>
2025-06-04 22:02:06 -0500

    modify loading animation to see it completatly

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -10,18 +10,22 @@ import { LoadingWord } from './LoadingWord';
 // @styles
 import './loading.scss';
 
-export const Loading = ({ voguerSize = 100, loaderHeight = 100 }) => {
+export const Loading = ({ voguerSize = 100, loaderHeight = 160 }) => {
   const { hide } = useSelector((state) => state.ui.loading);
+
   return (
     <div
       className="loading-container"
-      style={{ height: `${loaderHeight}px`, display: `${hide && 'none'}` }}
+      style={{
+        height: `${loaderHeight}px`,
+        display: hide ? 'none' : 'flex',
+      }}
     >
       <LoadingWord />
       <img
         src={voguerLoader}
         alt="voguer"
-        style={{ height: `${voguerSize}px`, width: `${voguerSize}px` }}
+        style={{ height: `${voguerSize}px`, width: `${voguerSize}px`, marginTop: '16px' }}
       />
     </div>
   );
