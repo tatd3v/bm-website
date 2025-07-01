4237642e2dbe1f29fe4b32c72c51d62be74f368a
tatd3v <tatdev91@gmail.com>
2025-07-01 01:23:16 -0500

    add mq for bigger displays

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -10,23 +10,18 @@ import { LoadingWord } from './LoadingWord';
 // @styles
 import './loading.scss';
 
-export const Loading = ({ voguerSize = 100, loaderHeight = 160 }) => {
-  const { hide } = useSelector((state) => state.ui.loading);
+export const Loading = () => {
+  const { hide } = useSelector(state => state.ui.loading);
 
   return (
     <div
       className="loading-container"
       style={{
-        height: `${loaderHeight}px`,
         display: hide ? 'none' : 'flex',
       }}
     >
       <LoadingWord />
-      <img
-        src={voguerLoader}
-        alt="voguer"
-        style={{ height: `${voguerSize}px`, width: `${voguerSize}px`, marginTop: '16px' }}
-      />
+      <img src={voguerLoader} alt="voguer" className="loading-voguer" />
     </div>
   );
 };
